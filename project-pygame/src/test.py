e18eee8dceeb2d2352ba586b3136d7902c550cd6
Nacho <nachoarriaga@outlook.com>
2023-10-18 12:58:24 -0300

    agregando cambios

--- project-pygame/src/test.py
+++ project-pygame/src/test.py
@@ -9,16 +9,16 @@ from colisiones import *
 # inicializar los modulos de pygame
 pygame.init()
 
+
 def terminar():
     pygame.quit()
     exit()
 
+
 def mostrar_texto(texto, fuente, coordenadas, color_fuente, color_fondo):
     texto = fuente.render
-    
+
     pass
-
-
 
 
 def wait_user():
@@ -26,11 +26,12 @@ def wait_user():
         for evento in pygame.event.get():
             if evento.type == QUIT:
                 terminar()
-            
+
             if evento.type == KEYDOWN:
                 if evento.key == K_ESCAPE:
                     terminar()
                 return
+
 
 # config de la pantalla principal
 pygame.display.set_caption("Game Test")
@@ -63,34 +64,38 @@ pygame.time.set_timer(EVENT_NEW_COIN, 3000)
 contador = 0
 contador_gde = 0
 
-# movimiento 
+# movimiento
 
 move_left = False
 move_right = False
 move_down = False
 move_up = False
 
-def crear_bloque(imagen=None, left=0, top=0, width=BLOCK_WIDTH, height=BLOCK_HEIGHT, color=white, dir=3, borde=0, radio = -1, speed_x=5, speed_y=5,):
+
+def crear_bloque(imagen=None, left=0, top=0, width=BLOCK_WIDTH, height=BLOCK_HEIGHT, color=white, dir=3, borde=0, radio=-1, speed_x=5, speed_y=5,):
     rec = pygame.Rect(left, top, width, height)
     if imagen:
         imagen = pygame.transform.scale(imagen, (width, height))
 
     return {"rect": rec, "color": color, "dir": dir, "borde": borde, "radio": radio, "speed_x": speed_x, "speed_y": speed_y, "imagen": imagen}
 
+
 def handler_new_coin():
     pass
+
 
 def load_coin_list(coins, cantidad):
     for i in range(cantidad):
         size_coin = randint(size_min_coin, size_max_coin)
-        coins.append(crear_bloque(None, randint(0, WIDTH - size_coin), randint(0, HEIGHT - size_coin), size_coin, size_coin, yellow, radio=size_coin // 2))
+        coins.append(crear_bloque(None, randint(0, WIDTH - size_coin), randint(0,
+                     HEIGHT - size_coin), size_coin, size_coin, yellow, radio=size_coin // 2))
 
-def dibujar_asteroides(superficie,coins):
+
+def dibujar_asteroides(superficie, coins):
     for coin in coins:
         if coin["imagen"]:
             pass
     pass
-
 
 
 # rectangulos
@@ -104,17 +109,18 @@ block = crear_bloque()
 
 for i in range(1):
 
-        rect = pygame.Rect( randint(0, WIDTH - BLOCK_WIDTH),
-                            randint(0, HEIGHT - BLOCK_HEIGHT), 
-                            BLOCK_WIDTH, 
-                            BLOCK_HEIGHT)
-        color = generate_random_color()
-        dir = get_random_direction()
-        speed_x = randint(5,15)
-        speed_y = randint(5,15)
+    rect = pygame.Rect(randint(0, WIDTH - BLOCK_WIDTH),
+                       randint(0, HEIGHT - BLOCK_HEIGHT),
+                       BLOCK_WIDTH,
+                       BLOCK_HEIGHT)
+    color = generate_random_color()
+    dir = get_random_direction()
+    speed_x = randint(5, 15)
+    speed_y = randint(5, 15)
 
-        block_dict = {"rect": rect, "color": color, "dir": dir, "speed_x": speed_x, "speed_y": speed_y}
-        bloques.append(block_dict)
+    block_dict = {"rect": rect, "color": color,
+                  "dir": dir, "speed_x": speed_x, "speed_y": speed_y}
+    bloques.append(block_dict)
 
 # crear lista de coins
 coins = []
@@ -132,7 +138,7 @@ while is_running:
 
         if e.type == EVENT_NEW_COIN:
             pass
-        
+
         # evento al presionar la tecla
         if e.type == pygame.KEYDOWN:
             # teclas de movimiento WASD y flechas
@@ -166,7 +172,8 @@ while is_running:
 
         if e.type == MOUSEBUTTONDOWN:
             if e.button == 1:
-                new_coin = (crear_bloque( e.pos[0] , e.pos[1], size_coin, cyan, radio= size_coin // 2))
+                new_coin = (crear_bloque(
+                    e.pos[0], e.pos[1], size_coin, color=cyan, radio=size_coin // 2))
                 new_coin["rect"].left -= size_coin // 2
                 new_coin["rect"].top -= size_coin // 2
                 coins.append(new_coin)
@@ -223,34 +230,42 @@ while is_running:
             # ABAJO
             block["rect"].top += speed_y
 
+        # movemos los asteroides
+
+        for coin in coins:
+            coin["rect"].move_ip(0, coin["speed_y"])
+
+        # reaparecerlos desde arriba de la pantalla
+
+        for coin in coins:
+            coin["rect"].move_ip(0, - size_coin)
+
     for coin in coins[:]:
         if coin["imagen"]:
             VENTANA.blit(coin["imagen"], coin["rect"])
         else:
-            pygame.draw.rect()
-        # if detectar_colision_circ(coin["rect"], block["rect"]):
-        #     coins.remove(coin)
-        #     contador += 1
-        #     # texto = fuente.render(f"Coins : {contador}", True, magenta)
-        #     # rect_texto = texto.get_rect()
-        #     # rect_texto.midtop = 
-        
-        # VENTANA.blit(coin["imagen", coin ["rect"]])
-        pass
+            # pygame.draw.rect()
+            # if detectar_colision_circ(coin["rect"], block["rect"]):
+            #     coins.remove(coin)
+            #     contador += 1
+            #     # texto = fuente.render(f"Coins : {contador}", True, magenta)
+            #     # rect_texto = texto.get_rect()
+            #     # rect_texto.midtop =
+
+            # VENTANA.blit(coin["imagen", coin ["rect"]])
+            pass
 
     # -----> Dibujar pantalla
 
-    VENTANA.fill(background, (0,0))
+    VENTANA.fill(black)
     for coin in coins:
-        pygame.draw.rect(VENTANA, coin["color"], coin["rect"], coin["borde", coin["radio"]])
+        pygame.draw.rect(VENTANA, coin["color"],
+                         coin["rect"], coin["borde", coin["radio"]])
 
     for block in bloques:
         pygame.draw.rect(VENTANA, block["color"], block["rect"])
 
     # -----> Actualizar pantalla
     pygame.display.flip()
-    
-pygame.quit()
-    
 
-   
+pygame.quit()
